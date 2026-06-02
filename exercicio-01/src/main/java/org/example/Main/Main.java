fc65ae2c08c7b6c60873eca6f7d7a5117265b344
Luiz Carlos Martendal <luiz.c.martendal@hotmail.com>
2026-06-02 20:33:44 -0300

    finalizando questão 01

--- exercicio-01/src/main/java/org/example/Main/Main.java
+++ exercicio-01/src/main/java/org/example/Main/Main.java
@@ -5,7 +5,7 @@ import org.example.exercicios.exer01.Item;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.example.exercicios.exer01.Algoritmo.algoritmo;
+import static org.example.exercicios.exer01.Algoritmo.algoritmoPreencherComMaximoDeItensPRO;
 
 public class Main {
 
@@ -19,12 +19,13 @@ public class Main {
         itensDisponiveis.add(new Item(5,15));
         int qtdeMaximaCarregamento = 100;
 
-        var itensCarregados = algoritmo(qtdeMaximaCarregamento, itensDisponiveis);
+        var itensCarregados = algoritmoPreencherComMaximoDeItensPRO(qtdeMaximaCarregamento, itensDisponiveis);
         for (Item i : itensCarregados) {
             System.out.println("Item: " + i.id + " com valor "+ i.valor +" carregado");
         }
 
-
+        //Seu algoritmo é capaz de retornar a solução ótima?
+        //R: O mais próximo, mas é custoso, pois é testado o próximo item a ser posto para fechar o mais próximo de 100.
         // ---
 
 
--- exercicio-01/src/main/java/org/example/exercicios/exer01/Algoritmo.java
+++ exercicio-01/src/main/java/org/example/exercicios/exer01/Algoritmo.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 public class Algoritmo {
 
-    public static List<Item> algoritmo(int capacidadeVeiculo, List<Item> itensDisponiveis) {
+    public static List<Item> algoritmoPreencherComMaximoDeItens(int capacidadeVeiculo, List<Item> itensDisponiveis) {
 
-        ordenarLista(itensDisponiveis);
+        itensDisponiveis.sort((a, b) -> a.valor - b.valor);
 
         List<Item> itensParaCarregar = new ArrayList<>();
 
@@ -21,22 +21,72 @@ public class Algoritmo {
             Item itemAtual = itensDisponiveis.get(i);
 
             while ((qtdeCarregada + itemAtual.valor) <= capacidadeVeiculo) {
-
                 itensParaCarregar.add(itemAtual);
-
                 qtdeCarregada += itemAtual.valor;
             }
-
-            if (qtdeCarregada < capacidadeVeiculo) qtdeCarregada -= itemAtual.valor;
         }
 
         return itensParaCarregar;
     }
 
-    private static List<Item> ordenarLista(List<Item> itens) {
-        Collections.sort(itens, (a, b) -> a.valor - b.valor);
+    public static List<Item> algoritmoPreencherComMaximoDeItensPRO(int capacidadeVeiculo, List<Item> itensDisponiveis) {
 
-        return itens;
+        itensDisponiveis.sort((a, b) -> a.valor - b.valor);
+
+        List<Item> itensParaCarregar = new ArrayList<>();
+
+        int qtdeCarregada = 0;
+
+        for (int i = 0; i < itensDisponiveis.size(); i++) {
+
+            if (qtdeCarregada == capacidadeVeiculo) break;
+
+            Item itemAtual = itensDisponiveis.get(i);
+
+            while ((qtdeCarregada + itemAtual.valor) <= capacidadeVeiculo) {
+                itensParaCarregar.add(itemAtual);
+                qtdeCarregada += itemAtual.valor;
+            }
+
+            Item proximoItem = null;
+            if (itensDisponiveis.size()-1 > i+1) {
+                proximoItem = itensDisponiveis.get(i+1);
+            }
+
+
+            if (qtdeCarregada < capacidadeVeiculo) {
+                if (proximoItem != null
+                        && (qtdeCarregada - itemAtual.valor + proximoItem.valor) <= capacidadeVeiculo
+                ) {
+                    qtdeCarregada -= itemAtual.valor;
+                    itensParaCarregar.remove(itemAtual);
+                }
+            }
+        }
+
+        return itensParaCarregar;
     }
 
+    public static List<Item> algoritmoPreencherComMinimoDeItens(int capacidadeVeiculo, List<Item> itensDisponiveis) {
+
+        itensDisponiveis.sort((a, b) -> b.valor - a.valor);
+
+        List<Item> itensParaCarregar = new ArrayList<>();
+
+        int qtdeCarregada = 0;
+
+        for (int i = 0; i < itensDisponiveis.size(); i++) {
+
+            if (qtdeCarregada == capacidadeVeiculo) break;
+
+            Item itemAtual = itensDisponiveis.get(i);
+
+            while ((qtdeCarregada + itemAtual.valor) <= capacidadeVeiculo) {
+                itensParaCarregar.add(itemAtual);
+                qtdeCarregada += itemAtual.valor;
+            }
+        }
+
+        return itensParaCarregar;
+    }
 }
